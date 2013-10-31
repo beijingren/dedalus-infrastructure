354f39ddf26b0abb5b952ff39d18ad76f2698117
David Höppner <0xffea@gmail.com>
2013-10-31 16:20:21 +0100

    Fix postgres packages and user

--- scripts/postgres-start.sh
+++ scripts/postgres-start.sh
@@ -3,9 +3,9 @@
 pg_ctlcluster 9.1 main start
 
 su postgres -c psql <<EOL
-create role docker nosuperuser nocreatedb;
+create role docker superuser login;
 EOL
 
-su postgres -c "createdb docker"
+su postgres -c "createdb -O docker docker""
 
 tail -f /var/log/postgresql/postgresql-9.1-main.log
--- scripts/provision-django.sh
+++ scripts/provision-django.sh
@@ -6,7 +6,11 @@ docker build -t 0xffea/raring-server-django - <<EOL
 FROM 0xffea/raring-server-cloudimg-amd64
 MAINTAINER David Höppner <0xffea@gmail.com>
 
-RUN apt-get -y install python-django apache2 postgresql-client-common
+RUN apt-get -y install	\
+	python-django	\
+	apache2		\
+	libapache2-mod-wsgi	'\
+	postgresql-client-common
 
 EXPOSE 80
 

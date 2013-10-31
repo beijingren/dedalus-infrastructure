dc3ba5cbc4b9ddcb35580bcc69b7408a49ba14aa
David Höppner <0xffea@gmail.com>
2013-10-31 14:50:41 +0100

    Add role and database

--- scripts/postgres-start.sh
+++ scripts/postgres-start.sh
@@ -2,4 +2,10 @@
 
 pg_ctlcluster 9.1 main start
 
+su postgres -c psql <<EOL
+create role docker nosuperuser nocreatedb;
+EOL
+
+su postgres -c "createdb docker"
+
 tail -f /var/log/postgresql/postgresql-9.1-main.log

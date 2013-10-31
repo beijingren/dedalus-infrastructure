1b942ca900f2654b66e0ba5d1f6d50198068494e
David Höppner <0xffea@gmail.com>
2013-10-31 15:42:52 +0100

    Start apache in django container

---
+++ scripts/django-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/etc/init.d/apache2 start
+
+tail -f /var/log/apache2/error.log
--- scripts/provision-django.sh
+++ scripts/provision-django.sh
@@ -6,7 +6,12 @@ docker build -t 0xffea/raring-server-django - <<EOL
 FROM 0xffea/raring-server-cloudimg-amd64
 MAINTAINER David Höppner <0xffea@gmail.com>
 
-RUN apt-get -y install python-django apache2
+RUN apt-get -y install python-django apache2 postgresql-client-common
 
 EXPOSE 80
+
+ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/scripts/django-start.sh /django-start.sh
+RUN chmod 0755 /django-start.sh
+
+CMD ["/django-start.sh"]
 EOL

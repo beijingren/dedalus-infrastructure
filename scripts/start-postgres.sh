0c20dde59410fd418ceea6d721408b3f8eee2823
David Höppner <0xffea@gmail.com>
2013-11-13 16:14:37 +0100

    Remote postgres login

--- scripts/start-postgres.sh
+++ scripts/start-postgres.sh
@@ -2,9 +2,13 @@
 
 pg_ctlcluster 9.1 main start
 
+# XXX: fix password in production
 su postgres -c psql <<EOL
-create role docker superuser login;
+create role docker superuser createdb password 'docker' login;
 EOL
+
+echo "listen_addresses = '*'" >> /etc/postgresql/9.1/main/postgresql.conf
+echo "hostssl all     all     0.0.0.0/0       md5" >> /etc/postgresql/9.1/main/pg_hba.conf
 
 #su postgres -c "createdb -O docker docker"
 

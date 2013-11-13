dab13624569b23249c6dc7e93a83482235150ea6
David Höppner <0xffea@gmail.com>
2013-11-13 17:00:13 +0100

    Configure before starting the server

--- scripts/start-postgres.sh
+++ scripts/start-postgres.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# XXX: fix in production
+echo "listen_addresses = '*'" >> /etc/postgresql/9.1/main/postgresql.conf
+echo "hostssl all     all     0.0.0.0/0       md5" >> /etc/postgresql/9.1/main/pg_hba.conf
+
 pg_ctlcluster 9.1 main start
 
-# XXX: fix password in production
+# XXX: fix in production
 su postgres -c psql <<EOL
 create role docker superuser createdb password 'docker' login;
 EOL
-
-echo "listen_addresses = '*'" >> /etc/postgresql/9.1/main/postgresql.conf
-echo "hostssl all     all     0.0.0.0/0       md5" >> /etc/postgresql/9.1/main/pg_hba.conf
 
 #su postgres -c "createdb -O docker docker"
 

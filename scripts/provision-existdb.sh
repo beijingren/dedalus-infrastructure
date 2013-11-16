c8a9f768a8070764629ce5fd44d1965b8c5f8277
David Höppner <0xffea@gmail.com>
2013-11-16 14:51:12 +0100

    Load documents into existdb

--- scripts/provision-existdb.sh
+++ scripts/provision-existdb.sh
@@ -9,6 +9,10 @@ docker build -t 0xffea/saucy-server-existdb - <<EOL
 FROM 0xffea/saucy-server-existdb-amd64
 MAINTAINER David Höppner <0xffea@gmail.com>
 
+RUN export DEBIAN_FRONTEND=noninteractive
+RUN apt-get -qy install	\
+	git
+
 EXPOSE 8080 8443
 
 ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/scripts/start-existdb.sh /root/start-existdb.sh
--- scripts/start-existdb.sh
+++ scripts/start-existdb.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+#
+# eXist-db
+#
 
+cd /tmp
+
+# Get XML documents
+/usr/bin/git clone https://github.com/beijingren/dublin-store.git
+
+# Start the server
+# TODO: should be daemon mode
 /usr/local/existdb/tools/wrapper/bin/wrapper-linux-x86-64 -c ../conf/wrapper.conf
+
+/bin/sleep 10s
+
+# Load documents into the db
+# TODO: fix password in production
+for f in $(find /tmp/dublin-store -name "*.xml"); do
+/usr/local/existdb/tools/wrapper/bin/client.sh -u admin -P glen32 -m /db/docker -p /tmp/dublin-store/${f}
+done
+
+# TODO: should be server log
+/usr/bin/tail -f /usr/local/existdb/tools/wrapper/logs/wrapper.log

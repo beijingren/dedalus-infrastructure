83c2e18ecad46e3b22ef203caec8206c9e07fbdc
David Höppner <0xffea@gmail.com>
2013-11-27 12:20:14 +0100

    Seems docker 0.7.0 ADD does not overwrite files

--- scripts/provision-existdb.sh
+++ scripts/provision-existdb.sh
@@ -15,10 +15,10 @@ RUN apt-get -qy install	\
 
 EXPOSE 8080 8443
 
-ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/scripts/start-existdb.sh /root/start-existdb.sh
-RUN chmod 0755 /root/start-existdb.sh
+ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/scripts/start-existdb.sh /start-existdb.sh
+RUN chmod 0755 /start-existdb.sh
 
-CMD ["/root/start-existdb.sh"]
+CMD ["/start-existdb.sh"]
 EOL
 
 docker run -d -privileged -name existdb -p 8080:8080 -v /var/lib/volume1:/docker/volume1:rw -t 0xffea/saucy-server-existdb

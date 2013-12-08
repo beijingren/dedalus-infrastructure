92fada7d058836bb0bfc9c2e49e5f61dfce653a1
David Höppner <0xffea@gmail.com>
2013-12-08 18:16:59 +0100

    Load default ontology into fuseki server

--- scripts/start-fuseki.sh
+++ scripts/start-fuseki.sh
@@ -6,12 +6,12 @@
 
 cd /root
 
+# TODO: should be shared among containers (volume1)
+/usr/bin/git clone https://github.com/beijingren/dublin-store.git
+
 wget http://www.apache.org/dist/jena/binaries/jena-fuseki-1.0.0-distribution.tar.gz
 tar xf jena-fuseki-1.0.0-distribution.tar.gz
 cd jena-fuseki-1.0.0
 
 # Start the server
-./fuseki start
-
-# TODO
-tail -f NOTICE
+./fuseki-server --mem --update --file /root/dublin-store/rdf/sikuquanshu.rdf /ds

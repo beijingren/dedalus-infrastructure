f459f7d27dd04a243965d1fb7eafe5933b57706a
David Höppner <0xffea@gmail.com>
2013-12-11 13:22:50 +0100

    Correct var name

--- scripts/start-django.sh
+++ scripts/start-django.sh
@@ -26,7 +26,7 @@ echo "export DB_PORT_5432_TCP_PORT=${DB_PORT_5432_TCP_PORT}" >> /etc/apache2/env
 echo "export XMLDB_PORT_8080_TCP_ADDR=${XMLDB_PORT_8080_TCP_ADDR}" >> /etc/apache2/envvars
 echo "export XMLDB_PORT_8080_TCP_PORT=${XMLDB_PORT_8080_TCP_PORT}" >> /etc/apache2/envvars
 echo "export SPARQL_PORT_3030_TCP_ADDR=${SPARQL_PORT_3030_TCP_ADDR}" >> /etc/apache2/envvars
-echo "export SPARQL_PORT_8080_TCP_PORT=${SPARQL_PORT_3030_TCP_PORT}" >> /etc/apache2/envvars
+echo "export SPARQL_PORT_3030_TCP_PORT=${SPARQL_PORT_3030_TCP_PORT}" >> /etc/apache2/envvars
 
 cd /home/docker/roche-website
 

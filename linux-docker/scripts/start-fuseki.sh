8de853e7ebfe3837cd75ec5c29d2a294a23780e4
David Höppner <0xffea@gmail.com>
2014-02-12 00:49:04 +0800

    Update fuseki

--- linux-docker/scripts/start-fuseki.sh
+++ linux-docker/scripts/start-fuseki.sh
@@ -9,9 +9,9 @@ cd /root
 # TODO: should be shared among containers (volume1)
 /usr/bin/git clone https://github.com/beijingren/dublin-store.git
 
-wget http://www.apache.org/dist/jena/binaries/jena-fuseki-1.0.0-distribution.tar.gz
-tar xf jena-fuseki-1.0.0-distribution.tar.gz
-cd jena-fuseki-1.0.0
+wget http://www.apache.org/dist/jena/binaries/jena-fuseki-1.0.1-distribution.tar.gz
+tar xf jena-fuseki-1.0.1-distribution.tar.gz
+cd jena-fuseki-1.0.1
 
 # Start the server
 ./fuseki-server --mem --update --file /root/dublin-store/rdf/sikuquanshu.rdf /ds

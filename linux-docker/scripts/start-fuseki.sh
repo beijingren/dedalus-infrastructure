afa297b82accedff98c377f5c77aa2e8bd5a567b
David Höppner <0xffea@gmail.com>
2014-02-19 20:05:09 +0800

    Fuseki: load CCDB RDF data

--- linux-docker/scripts/start-fuseki.sh
+++ linux-docker/scripts/start-fuseki.sh
@@ -10,5 +10,13 @@ wget http://www.apache.org/dist/jena/binaries/jena-fuseki-1.0.1-distribution.tar
 tar xf jena-fuseki-1.0.1-distribution.tar.gz
 cd jena-fuseki-1.0.1
 
+chmod +x s-put
+
 # Start the server
-./fuseki-server --mem --update --file /docker/dublin-store/rdf/sikuquanshu.rdf /ds
+./fuseki-server --mem --update --file /docker/dublin-store/rdf/sikuquanshu.rdf /ds &
+
+sleep 4
+
+./s-put http://localhost:3030/ds/data default /docker/dublin-store/rdf/NamedIndividual.rdf
+
+tail -f /docker/dublin-store/rdf/NamedIndividual.rdf

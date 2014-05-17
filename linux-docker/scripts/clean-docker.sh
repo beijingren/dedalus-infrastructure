d2ea3aa79537caac53fd26aee51e5679550728e9
David Höppner <0xffea@gmail.com>
2014-05-17 19:05:33 +0200

    Clean up clean up script

--- linux-docker/scripts/clean-docker.sh
+++ linux-docker/scripts/clean-docker.sh
@@ -5,16 +5,13 @@ docker kill fuseki
 docker kill existdb
 docker kill postgres
 
-DOCKER_EXIT=$(docker ps -a | grep 'Exit')
-DOCKER_TRUNC=$(docker ps -a --no-trunc | grep 'Exit')
+docker rm django
+docker rm fuseki
+docker rm existdb
+docker rm postgres
 
-if [-n DOCKER_EXIT]; then
 docker ps -a | grep 'Exit' | awk '{print $1}' | sudo xargs docker rm
-fi
-
-if [ -n DOCKER_TRUNC]; then
 docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' | xargs -r docker rm
-fi
 
 docker rmi -f 0xffea/saucy-server-django
 docker rmi -f 0xffea/saucy-server-postgres

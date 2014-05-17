bb0b53430877d150811815ef3c0055653b01069d
David Höppner <0xffea@gmail.com>
2014-05-17 18:42:29 +0200

    And again

--- linux-docker/scripts/clean-docker.sh
+++ linux-docker/scripts/clean-docker.sh
@@ -5,8 +5,8 @@ docker kill fuseki
 docker kill existdb
 docker kill postgres
 
-docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm || true
-docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' | xargs -r docker rm || true
+(docker ps -a | grep Exit | awk '{print $1}' | sudo xargs docker rm) || true
+(docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' | xargs -r docker rm) || true
 
 docker rmi -f 0xffea/saucy-server-django
 docker rmi -f 0xffea/saucy-server-postgres

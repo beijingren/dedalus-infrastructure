60b51a9d1490730e55e6b821e450c977eda5ecfb
David Höppner <0xffea@gmail.com>
2014-08-25 19:03:16 +0200

    Update fuseki

--- linux-docker/scripts/start-fuseki.sh
+++ linux-docker/scripts/start-fuseki.sh
@@ -6,9 +6,9 @@
 
 cd /root
 
-wget http://www.apache.org/dist/jena/binaries/jena-fuseki-1.0.2-distribution.tar.gz
-tar xf jena-fuseki-1.0.2-distribution.tar.gz
-cd jena-fuseki-1.0.2
+wget http://www.apache.org/dist/jena/binaries/jena-fuseki-1.1.0-distribution.tar.gz
+tar xf jena-fuseki-1.1.0-distribution.tar.gz
+cd jena-fuseki-1.1.0
 
 chmod +x s-put
 

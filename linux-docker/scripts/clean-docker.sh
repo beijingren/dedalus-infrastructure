69549138e1a318ec03733b9da5a80af1c739a80f
David Höppner <0xffea@gmail.com>
2014-09-15 13:26:58 +0200

    Eemove running containers

--- linux-docker/scripts/clean-docker.sh
+++ linux-docker/scripts/clean-docker.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#docker kill django
-#docker kill fuseki
-#docker kill existdb
-#docker kill postgres
+docker kill django
+docker kill fuseki
+docker kill existdb
+docker kill postgres
 
-#docker rm django
-#docker rm fuseki
-#docker rm existdb
-#docker rm postgres
+docker rm django
+docker rm fuseki
+docker rm existdb
+docker rm postgres
 
 docker ps -a | grep 'Exit' | awk '{print $1}' | sudo xargs docker rm
 docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' | xargs -r docker rm
 
-#docker rmi -f 0xffea/saucy-server-django
-#docker rmi -f 0xffea/saucy-server-postgres
-#docker rmi -f 0xffea/saucy-server-fuseki
-#docker rmi -f 0xffea/saucy-server-existdb
+docker rmi -f 0xffea/saucy-server-django
+docker rmi -f 0xffea/saucy-server-postgres
+docker rmi -f 0xffea/saucy-server-fuseki
+docker rmi -f 0xffea/saucy-server-existdb
 
 exit 0

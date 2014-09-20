c4dbb38862be854b585036b0403a6a69e653eb63
David Höppner <0xffea@gmail.com>
2014-09-20 16:17:24 +0200

    UIMA worker

--- linux-docker/scripts/uima-worker.py
+++ linux-docker/scripts/uima-worker.py
@@ -1,15 +1,53 @@
 #!/usr/bin/env python
+# coding=utf-8
 
+import json
 import os
 import sys
+import tempfile
+import subprocess
 
 from pika import BasicProperties
 from pika import BlockingConnection
 from pika import ConnectionParameters
 
 
-def uima_callback(channel, method, properties, body):
+BERTIE_JAR = "/docker/bertie-uima/target/bertie-uima-0.0.1-SNAPSHOT.jar"
+
+
+def uima_callback(channel, method, props, body):
     print " [x] Received %r" % (body,)
+
+    uima = json.loads(body)
+
+    collection_path = None
+
+    text = uima['text']
+    lemma = uima['lemma']
+    function = uima['function']
+
+    os.chdir('/docker/dublin-store')
+    for (dirpath, dirnames, filenames) in os.walk(u'浙江大學圖書館'):
+        if dirpath.endswith(unicode(text)):
+            collection_path = '/docker/dublin-store/' + dirpath
+            break
+
+    # Build owl
+    owl_file = open("/docker/dublin-store/rdf/placename_fragment.rdf")
+    owl_fragment = owl_file.read().decode('utf-8')
+    owl_fragment = owl_fragment.format(lemma)
+
+    f = tempfile.NamedTemporaryFile(delete=False)
+    f.write(owl_fragment.encode('utf-8'))
+    f.close()
+
+    # Call UIMA analysis engine
+    result = subprocess.call(["/usr/bin/java", "-Dfile.encoding=UTF-8", "-jar", BERTIE_JAR,
+                              "--tei",
+                              "--directory", collection_path,
+                              "--owl", f.name])
+    # Remove tempfile
+    os.unlink(f.name)
 
     response = "UIMA"
     channel.basic_publish(exchange='',

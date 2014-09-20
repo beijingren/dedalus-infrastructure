fd02945430f3a476d66f56b2361fbf1457f1bea2
David Höppner <0xffea@gmail.com>
2014-09-20 10:19:19 +0200

    Use dockerfile

--- linux-docker/scripts/provision-celery.sh
+++ linux-docker/scripts/provision-celery.sh
@@ -5,29 +5,4 @@
 docker kill celery
 docker rm celery
 
-docker build -t 0xffea/saucy-server-celery - <<EOL
-FROM ubuntu:latest
-MAINTAINER David Höppner <0xffea@gmail.com>
-
-RUN echo "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
-
-RUN export DEBIAN_FRONTEND=noninteractive
-RUN apt-get update
-RUN apt-get install -y		\
-	python-celery		\
-	rabbitmq-server
-
-RUN useradd -m -p "celery" celery
-RUN locale-gen en_US.UTF-8
-RUN export LANG=en_US.UTF-8
-RUN echo "LANG=en_US.UTF-8" > /etc/default/locale
-
-EXPOSE 5672
-
-ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/linux-docker/scripts/start-celery.sh /home/celery/start-celery.sh
-RUN chmod 0755 /home/celery/start-celery.sh
-
-CMD ["/home/celery/start-celery.sh"]
-EOL
-
-docker run -d --privileged --name celery -p 5672:5672 -v /docker:/docker:rw -t 0xffea/saucy-server-celery
+docker run -d --privileged --name celery -p 5672:5672 -p 15672:15672 dockerfile/rabbitmq -v /docker:/docker:rw -t dockerfile/rabbitmq:latest

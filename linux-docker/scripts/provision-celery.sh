ec3e1f63f447a57320789519591fef0cfd35cca7
David Höppner <0xffea@gmail.com>
2014-09-20 10:30:28 +0200

    Fix args

--- linux-docker/scripts/provision-celery.sh
+++ linux-docker/scripts/provision-celery.sh
@@ -5,4 +5,4 @@
 docker kill celery
 docker rm celery
 
-docker run -d --privileged --name celery -p 5672:5672 -p 15672:15672 dockerfile/rabbitmq -v /docker:/docker:rw -t dockerfile/rabbitmq:latest
+docker run -d --privileged --name celery -p 5672:5672 -p 15672:15672 -v /docker:/docker:rw -t dockerfile/rabbitmq:latest

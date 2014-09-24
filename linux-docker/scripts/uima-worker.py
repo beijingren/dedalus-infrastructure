e2fa294af1353c220f7681a599fb0b81c8f928e4
David Höppner <0xffea@gmail.com>
2014-09-24 13:53:03 +0200

    Support annotation of single juan

--- linux-docker/scripts/uima-worker.py
+++ linux-docker/scripts/uima-worker.py
@@ -27,17 +27,17 @@ def uima_callback(channel, method, props, body):
                               body=response)
         channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    # Try decode JSON
+    # Try to decode JSON payload
     try:
         uima = json.loads(body)
-    except ValueError:
+        function = uima['function']
+        text = uima['text']
+        lemma = uima['lemma']
+        collection_path = uima['collection_path']
+        juan = int(uima['juan'])
+    except ValueError, KeyError:
         send_response("ERROR")
         return
-
-    function = uima['function']
-    text = uima['text']
-    lemma = uima['lemma']
-    collection_path = uima['collection_path']
 
     # Dummy elements
     # TODO: annotations should handle emtpy result properly
@@ -71,13 +71,32 @@ def uima_callback(channel, method, props, body):
     subprocess.call(["ssh-agent", "bash", "-c", "ssh-add /docker/github_rsa ; /usr/bin/git pull;"])
 
     # Call UIMA analysis engine
+    if not juan == -1:
+        file_name = "/%03d.xml" % (juan,)
+        result = subprocess.call(["/usr/bin/java", "-Dfile.encoding=UTF-8",
+                                  "-Djava.util.logging.config.file=/docker/bertie-uima/src/main/properties/Logger.properties",
+                                  "-jar", BERTIE_JAR,
+                                  "--tei",
+                                  "--file", collection_path + file_name,
+                                  "--owl", f.name])
+
+        # Send response early
+        send_response("OK")
+
     start_uima = time.time()
-    result = subprocess.call(["/usr/bin/java", "-Dfile.encoding=UTF-8", "-jar", BERTIE_JAR,
+    result = subprocess.call(["/usr/bin/java", "-Dfile.encoding=UTF-8",
+                              "-Djava.util.logging.config.file=/docker/bertie-uima/src/main/properties/Logger.properties",
+                              "-jar", BERTIE_JAR,
                               "--tei",
                               "--directory", collection_path,
                               "--owl", f.name])
     done_uima = time.time()
+    print "RUNTIME"
     print done_uima - start_uima
+
+    # No single juan was annotated, so send response now
+    if juan == -1:
+        send_repsonse("OK")
 
     # Remove tempfile
     os.unlink(f.name)
@@ -85,8 +104,6 @@ def uima_callback(channel, method, props, body):
     # Commit the mess
     subprocess.call(["/usr/bin/git", "commit", "-m", "UIMA " + lemma, "."])
     subprocess.call(["ssh-agent", "bash", "-c", "ssh-add /docker/github_rsa ; /usr/bin/git push;"])
-
-    send_response("UIMA")
 
 if __name__ == "__main__":
     #
@@ -96,6 +113,7 @@ if __name__ == "__main__":
         rabbitmq_host = os.environ['RABBITMQ_PORT_5672_TCP_ADDR']
     except KeyError:
         print "Can't find rabbitmq server. Giving up."
+        print "Please set RABBITMQ_PORT_5672_TCP_ADDR."
         sys.exit(-1)
 
     connection = BlockingConnection(ConnectionParameters(host=rabbitmq_host))

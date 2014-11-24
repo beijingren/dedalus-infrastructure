dc464d1272e21ad1c792cc72d113b8276ff86cdd
David Höppner <0xffea@gmail.com>
2014-11-24 15:48:09 +0100

    Update riak with erlang

--- linux-docker/scripts/provision-riak.sh
+++ linux-docker/scripts/provision-riak.sh
@@ -12,16 +12,22 @@ MAINTAINER David Höppner <0xffea@gmail.com>
 RUN export DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && apt-get -qy build-dep \
 	erlang
+RUN apt-get -qy install	\
+	curl
+
+WORKDIR /root
+RUN curl -O http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.2/ubuntu/trusty/riak_2.0.2-1_amd64.deb
+RUN dpkg -i riak_2.0.2-1_amd64.deb
 
 EXPOSE 10017
 
 RUN locale-gen en_US.UTF-8
 RUN echo "LANG=en_US.UTF-8" > /etc/default/locale
 
-ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/linux-docker/scripts/start-postgres.sh /root/start-postgres.sh
-RUN chmod 0755 /root/start-postgres.sh
+ADD https://raw.github.com/beijingren/dedalus-infrastructure/master/linux-docker/scripts/start-riak.sh /root/start-riak.sh
+RUN chmod 0755 /root/start-riak.sh
 
-CMD ["/root/start-postgres.sh"]
+CMD ["/root/start-riak.sh"]
 EOL
 
 docker run -d --name riak -e LANG="en_US.UTF-8" -p 10017:10017 -v /docker:/docker:rw -t 0xffea/riak
--- linux-docker/scripts/start-riak.sh
+++ linux-docker/scripts/start-riak.sh
@@ -8,11 +8,22 @@ export LANG="en_US.UTF-8"
 
 cd /root
 
-wget http://www.erlang.org/download/otp_src_17.3.tar.gz
+curl -O http://www.erlang.org/download/otp_src_17.3.tar.gz
 tar xf otp_src_17.3.tar.gz
 
 cd otp_src_17.3
 ./configure
 make install
+
+#
+# Elixir
+#
+cd /root
+curl -LO https://github.com/elixir-lang/elixir/archive/v1.0.2.tar.gz
+tar xf v1.0.2.tar.gz
+cd elixir-1.0.2
+make install
+
+/usr/sbin/riak start
 
 tail -f /docker/dublin-store/rdf/NamedIndividual.rdf
